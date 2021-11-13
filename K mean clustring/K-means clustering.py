e3904f4c51445732254620ddf35dd8081046bf21
SophieSUN88 <85892147+SophieSUN88@users.noreply.github.com>
2021-11-13 12:25:04 -0600

    commit to 'master'

--- K mean clustring/K-means clustering.py
+++ K mean clustring/K-means clustering.py
@@ -4,7 +4,7 @@
 def initialize_centroids(data, k =2):
     Centroids =[]
     for _ in range(k):
-        i = np.random.randint(len(X))
+        i = np.random.randint(len(data))
         Centroids.append(X[i,:])
     return Centroids
 
@@ -47,3 +47,41 @@ def mse_classes(data, Centroids):
         error = sum(0.5*(centroid - a) @ (centroid - a) for a in A_classes[i])
         errors.append(error)    
     return sum(x for x in errors)
+
+
+# example
+# import some data to play with
+iris = datasets.load_iris()
+X = iris.data
+# we only need the first 2 columns
+X= X[:,:2]
+X.shape
+
+C1 = fit(X,k=1)
+C2 = fit(X,k=2)
+C3 = fit(X,k=3)
+C4 = fit(X,k=4)
+C5 = fit(X,k=5)
+C6 = fit(X,k=6)
+C=[C1, C2,C3,C4,C5,C6]
+
+errors = [mse_classes(X,centroids) for centroids in C]
+# plot the progress of the improvement in errors 
+errors = [mse_classes(X,centroids) for centroids in C]
+plt.figure(figsize=(10,6))
+plt.plot([i for i in range(1,7)],errors)
+
+# plot the data
+for x in X:
+    if assignment(x,C6) == 0:
+        plt.scatter(x[0],x[1],color ="blue")
+    elif assignment(x,C6) == 1:
+        plt.scatter(x[0],x[1],color ="green")
+    elif assignment(x,C6) == 2:
+        plt.scatter(x[0],x[1],color ="yellow")
+    elif assignment(x,C6) == 3:
+        plt.scatter(x[0],x[1],color ="purple")
+    elif assignment(x,C6) == 4:
+        plt.scatter(x[0],x[1],color ="black")
+    else:
+        plt.scatter(x[0],x[1],color ="red")
